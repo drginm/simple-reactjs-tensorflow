8399c8e0895ab792eb7b9889aa71684ff948ba3a
drginm <drginm>
2020-01-18 14:54:27 -0500

    Static site for - simple tensorflojs + react app

---
+++ precache-manifest.977a78b28f698726a91af79112d7e6b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed10e1eda802144747ca4211e30b20eb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ae30aaafe7e961a1425b",
+    "url": "./static/css/main.5b3a4915.chunk.css"
+  },
+  {
+    "revision": "436453293a1ec660748d",
+    "url": "./static/js/2.8f09fba7.chunk.js"
+  },
+  {
+    "revision": "5aa85970dae6bc409bed18591ac972a6",
+    "url": "./static/js/2.8f09fba7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ae30aaafe7e961a1425b",
+    "url": "./static/js/main.7746d645.chunk.js"
+  },
+  {
+    "revision": "333ada3a3e446f38f82d",
+    "url": "./static/js/runtime-main.dbecf0d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
